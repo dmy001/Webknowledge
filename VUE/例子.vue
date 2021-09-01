35f661bdbe5380b3c45fa8d08b5b1ae9797bbba8
note <dmy3040500@163.com>
2021-09-01 16:26:18 +0800

    /

--- VUE/例子.vue
+++ VUE/例子.vue
@@ -1,18 +1,14 @@
 <template>
   <div>
-      <h1>app组件</h1>
-      <p>姓:{{firstname}}</p>
-      <p>名:{{lastname}}</p>
-      
+    <h1>app组件</h1>
+    <p>姓:{{ firstname }}</p>
+    <p>名:{{ lastname }}</p>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style> 
